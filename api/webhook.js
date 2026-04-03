ecd885603ad69afeac4432613f83e69df4b9de80
adamgwakefield <64936671+adamgwakefield@users.noreply.github.com>
2026-04-03 14:55:23 -0400

    Add files via upload

--- api/webhook.js
+++ api/webhook.js
@@ -24,7 +24,7 @@ module.exports = async function(req, res) {
     `;
 
     // Instead of dealing with Google's buggy SDK package, we just hit their API directly!
-    const response = await fetch(\`https://generativelanguage.googleapis.com/v1beta/models/gemini-1.5-flash:generateContent?key=\${apiKey}\`, {
+    const response = await fetch(`https://generativelanguage.googleapis.com/v1beta/models/gemini-1.5-flash:generateContent?key=${apiKey}`, {
       method: 'POST',
       headers: { 'Content-Type': 'application/json' },
       body: JSON.stringify({
